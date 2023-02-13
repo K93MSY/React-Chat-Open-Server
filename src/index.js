e9ab189936eca12083097565cfb1a70ab46df7c9
K93MSY <masaya93@ogaki-tv.ne.jp>
2023-02-13 11:06:03 +0900

    Fix Header

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 require('dotenv').config();
 require('express')()
-.get('/',(req,res) => {
-    res.status(200);
-    res.send({
-        message:"Hello World"
+    .use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*')
+        res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+        res.header(
+          'Access-Control-Allow-Headers',
+          'Content-Type, Authorization, access_token'
+        )
+      
+        // intercept OPTIONS method
+        if ('OPTIONS' === req.method) {
+          res.send(200)
+        } else {
+          next()
+        }}
+    )
+    .get('/', (req, res) => {
+        res.status(200);
+        res.send({
+            message: "Hello World"
+        })
+    }).get('/users', (req, res) => {
+        res.send({
+            ActiveUsers: 0,
+        });
+    }).listen(process.env.PORT, () => {
+        console.log('Serving at' + process.env.PORT)
     })
-}).get('/users',(req,res) => {
-    res.send({
-        ActiveUsers:0,
-    });
-}).listen(process.env.PORT,()=>{
-    console.log('Serving at'+process.env.PORT)
-})
